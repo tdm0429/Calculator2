7bfe197c6e1ce64ba82c05e844a63da3e7aae4d2
David Hovemeyer <dhovemey@ycp.edu>
2013-02-06 11:24:32 -0500

    added a message

--- Calculator2/src/edu/ycp/cs320/calculator/Main.java
+++ Calculator2/src/edu/ycp/cs320/calculator/Main.java
@@ -3,5 +3,7 @@ package edu.ycp.cs320.calculator;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the calculator");
+		System.out.println("Fasten your seatbelt!");
+		
 	}
 }
