d9092bf6b31511ae9969a656eb71353a44c23004
Tim Berger <tberger3@ycp.edu>
2013-02-06 11:47:15 -0500

    message

--- Calculator2/src/edu/ycp/cs320/calculator/Main.java
+++ Calculator2/src/edu/ycp/cs320/calculator/Main.java
@@ -3,6 +3,5 @@ package edu.ycp.cs320.calculator;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the calculator");
-		System.out.println("Calculate some numbers");
 	}
 }
