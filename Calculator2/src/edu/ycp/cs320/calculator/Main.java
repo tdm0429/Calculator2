f3b1b888ce94a26cfd9d8d7e64ea4c433b315a19
Colin Bennett <cbennet1@ycp.edu>
2013-02-06 11:37:22 -0500

    Message

--- Calculator2/src/edu/ycp/cs320/calculator/Main.java
+++ Calculator2/src/edu/ycp/cs320/calculator/Main.java
@@ -3,7 +3,6 @@ package edu.ycp.cs320.calculator;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the calculator");
-		System.out.println("Fasten your seatbelt!");
-		
+		//
 	}
 }
