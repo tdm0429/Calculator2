5c252a3ec6ad1b176db79a1ac7ce274ce56d93ae
David Hovemeyer <dhovemey@ycp.edu>
2013-02-06 11:30:53 -0500

    allow user to enter input values

--- Calculator2/src/edu/ycp/cs320/calculator/Main.java
+++ Calculator2/src/edu/ycp/cs320/calculator/Main.java
@@ -1,9 +1,17 @@
 package edu.ycp.cs320.calculator;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the calculator");
 		System.out.println("Fasten your seatbelt!");
+		
+		Scanner keyboard = new Scanner(System.in);
+		System.out.print("Enter two numbers: ");
+		
+		double a = keyboard.nextDouble();
+		double b = keyboard.nextDouble();
 		
 	}
 }
