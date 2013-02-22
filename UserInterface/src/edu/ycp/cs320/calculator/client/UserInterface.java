10bb2532ac822c803ebaa2c77c84bfbd4a32dc70
TDM0429 <tmille33@ycp.edu>
2013-02-22 11:59:00 -0500

    UI Working

---
+++ UserInterface/src/edu/ycp/cs320/calculator/client/UserInterface.java
@@ -0,0 +1,22 @@
+package edu.ycp.cs320.calculator.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.client.ui.LayoutPanel;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
+
+public class UserInterface implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		LayoutPanel panel = new LayoutPanel();
+		
+		Game_Selection gameSelection = new Game_Selection();
+		panel.add(gameSelection);
+		
+		RootLayoutPanel.get().add(panel);
+		RootLayoutPanel.get().setWidgetLeftRight(panel, 0.0, Unit.PX, 0.0, Unit.PX);
+		RootLayoutPanel.get().setWidgetTopBottom(panel, 0.0, Unit.PX, 0.0, Unit.PX);
+	}
+
+}
