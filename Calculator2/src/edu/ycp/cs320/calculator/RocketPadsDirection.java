ad45c958609c46d91bc327d39dc3e5b95c1871b4
Colin Bennett <cbennet1@ycp.edu>
2013-02-22 11:14:22 -0500

    Reworked version of RocketPadsDirection. It's now an enumeration class.

--- Calculator2/src/edu/ycp/cs320/calculator/RocketPadsDirection.java
+++ Calculator2/src/edu/ycp/cs320/calculator/RocketPadsDirection.java
@@ -1,32 +1,12 @@
 package edu.ycp.cs320.calculator;
 
-public class RocketPadsDirection 
+public enum RocketPadsDirection 
 {
-	private int dx, dy;
-	
-	RocketPadsDirection(int dX, int dY)
-	{
-		dx = dX;
-		dy = dY;
-	}
-	
-	public int getDX()
-	{
-		return dx;
-	}
-	
-	public int getDY()
-	{
-		return dy;
-	}
-	
-	public void setXDir(int xdir)
-	{
-		dx = xdir;
-	}
-	
-	public void setYDir(int ydir)
-	{
-		dy = ydir;
-	}
+	NORTH,
+	SOUTH,
+	EAST,
+	WEST,
+	WALK,
+	STOP,
+	WIN;
 }
