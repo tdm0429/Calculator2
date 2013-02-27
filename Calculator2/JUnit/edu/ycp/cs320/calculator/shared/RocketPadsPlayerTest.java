144c9090647e63c3c6740c3a549b9727314c8a05
TDM0429 <tmille33@ycp.edu>
2013-02-27 12:08:33 -0500

    ... new

---
+++ Calculator2/JUnit/edu/ycp/cs320/calculator/shared/RocketPadsPlayerTest.java
@@ -0,0 +1,49 @@
+package edu.ycp.cs320.calculator.shared;
+
+import junit.framework.TestCase;
+
+public class RocketPadsPlayerTest extends TestCase
+{
+	private RocketPadsPlayer testPlayer;
+
+	@Override
+	protected void setUp() throws Exception 
+	{
+		testPlayer = new RocketPadsPlayer();
+	}
+
+	public void testGetLocation() throws Exception
+	{
+		RocketPadsLocation test_loc = new RocketPadsLocation(0,0);
+		assertEquals(1, test_loc.compareLocTo(testPlayer.getLocation()));
+	}
+
+	public void testSetLocation() throws Exception
+	{
+		RocketPadsLocation test_loc = new RocketPadsLocation(10,10);
+		testPlayer.setLocation(10, 10);
+		assertEquals(1, test_loc.compareLocTo(testPlayer.getLocation()));
+	}
+
+	public void testResetLocation() throws Exception
+	{
+		assertEquals(0, testPlayer.getLocation().getX());
+		assertEquals(0, testPlayer.getLocation().getY());
+	}
+
+	public void testGetSlide() throws Exception
+	{
+		assertFalse(testPlayer.getSlide());
+	}
+
+	public void testSetSlide() throws Exception
+	{
+		testPlayer.setSlide(true);
+		assertTrue(testPlayer.getSlide());
+	}
+
+	public void testUpdatePosition() throws Exception
+	{
+		//
+	}
+}
